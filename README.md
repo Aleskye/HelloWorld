3f9265d32172c91de0de94780952d0222e2afabc
Alexis Piquet <alexis.piquet-pellorce@epitech.eu>
2017-05-31 16:16:30 +0200

    Hello World

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HelloWorld
+# Ceci est juste un test.
+# Premier pas sur Github.
---
+++ main.c
@@ -0,0 +1,12 @@
+/*
+** Header
+** Project HelloWorld by Alexis P 
+*/
+
+#include <stdio.h>
+
+int	main(void)
+{
+  printf("Hello World\n");
+  return (0);
+}
